a8d7bb58abad51351be9abc9e1a5a128513897f1
Rudar <rudar6511@gmail.com>
2026-07-28 20:35:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const cards=document.querySelectorAll(".card");
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity=1;
+entry.target.style.transform="translateY(0)";
+
+}
+
+});
+
+});
+
+cards.forEach(card=>{
+
+card.style.opacity=0;
+card.style.transform="translateY(40px)";
+card.style.transition="0.6s";
+
+observer.observe(card);
+
+});
